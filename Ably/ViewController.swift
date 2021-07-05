fd66ba42850f0c8fff8a71291da1cbef5e898da3
soobin-k <tnqls27qks@naver.com>
2021-07-06 04:19:40 +0900

    week1 Ably project

---
+++ Ably/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Ably
+//
+//  Created by 김수빈 on 2021/06/26.
+//
+
+/*
+ View의 생명주기
+ 1. view
+ */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    // 어디엔가 로드 메모리
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       
+    }
+    
+
+
+
+}
+
