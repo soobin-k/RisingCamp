9258049161264ce5a9458c112153958b153c47f2
soobin-k <tnqls27qks@naver.com>
2021-07-19 01:32:31 +0900

    Initial Commit

---
+++ MovieInfoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieInfoApp
+//
+//  Created by 김수빈 on 2021/07/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
