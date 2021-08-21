19b89eca3f6067a29f3c6ae9b44863e6ac48abf0
soobin-k <tnqls27qks@naver.com>
2021-08-21 22:42:25 +0900

    Edit CollectionView Index Controll

--- MovieInfoApp/Sources/VCs/ListViewController.swift
+++ MovieInfoApp/Sources/VCs/ListViewController.swift
@@ -14,7 +14,7 @@ import XLPagerTabStrip
 
 class ListViewController: UIViewController, IndicatorInfoProvider, UICollectionViewDelegate, CollectionViewCellDelegate{
     func collectionView(collectionviewcell: MovieSingleCell?, index: Int, didTappedInTableViewCell: MovieListCell) {
-        print("어쩌구")
+        
         let movieVO = MovieVO.shared
         movieVO.index = index
         guard let vc = storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as? DetailViewController else { return }
